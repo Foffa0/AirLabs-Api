b2518f3165c28d6826a990f8381aa4d584b88a41
Foffa <schmuck.christopher@gmail.com>
2023-02-24 20:26:32 +0100

    new forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 class AirportForm(FlaskForm):
     query = StringField('Airport', validators=[DataRequired()])
     submit = SubmitField('Speichern')
+
+class AircraftForm(FlaskForm):
+    search_type = SelectField('Search Type', choices=[('0','Aircraft Code'),('1','Manufacturer'),('2','Engine count'),('3','3'),('4','4'),('5', '5'),])
+    query = StringField('Search Aircraft', validators=[DataRequired()])
