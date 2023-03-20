b8ea30888e7946d2c5727c156866b1f73dd1805a
Foffa <schmuck.christopher@gmail.com>
2023-03-20 17:49:08 +0100

    add submit btn

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,5 +7,5 @@ class AirportForm(FlaskForm):
     submit = SubmitField('Speichern')
 
 class AircraftForm(FlaskForm):
-    search_type = SelectField('Search Type', choices=[('0','Aircraft Code'),('1','Manufacturer'),('2','Engine count'),('3','3'),('4','4'),('5', '5'),])
     query = StringField('Search Aircraft', validators=[DataRequired()])
+    submit = SubmitField('Search')
