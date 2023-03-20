4489126f50e943e81363a6e4ce527e971e5d4141
Foffa <schmuck.christopher@gmail.com>
2023-03-20 17:48:46 +0100

    arrival and landing class

---
+++ app/data/schedule.py
@@ -0,0 +1,15 @@
+
+class ArrivalOrLanding:
+    def __init__(self, flightnumber, icao, aircraft, time, type):
+        """
+        :param flightnumber: the flightnumber of this flight
+        :param aircraft_icao: aircraft type in icao format
+        :param aircraft: full aircraft name
+        :param time: arrival or departure time in unix format
+        :param type: arrival -->0 or departure -->1
+        """
+        self.flightnumber = flightnumber
+        self.aircraft_icao = icao
+        self.aircraft = aircraft
+        self.time = time
+        self.type = type
