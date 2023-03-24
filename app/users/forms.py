a3da26d376ec471bf4a1c9332553762757cdee83
Foffa <schmuck.christopher@gmail.com>
2023-03-24 17:24:15 +0100

    account forms

---
+++ app/users/forms.py
@@ -0,0 +1,33 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
+from app.models import User
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Passwort', validators=[DataRequired()])
+    confirm_password = PasswordField('Passwort bestätigen', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Registrieren')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email existiert bereits. Bitte wähle eine andere!')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Passwort', validators=[DataRequired()])
+    remember = BooleanField('Angemeldet bleiben')
+    submit = SubmitField('Einloggen')
+
+class UpdateAccountForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Aktualisieren')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email existiert bereits. Bitte wähle eine andere!')
