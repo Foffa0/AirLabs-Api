4503be7796e564ba375727a34c5b0b961d69f6ce
Foffa <schmuck.christopher@gmail.com>
2023-04-03 23:15:42 +0200

    let only admins refresh the gmail token

--- app/users/utils.py
+++ app/users/utils.py
@@ -9,6 +9,7 @@ import base64
 from email.message import EmailMessage
 from email.mime.text import MIMEText
 import pickle
+from app.models import User
 
 
 def send__email(user, type):
@@ -32,19 +33,20 @@ def send__email(user, type):
 
     # Read the token from the file and store it in the variable creds
     with open('token.pickle', 'rb') as token:
-        creds = pickle.load(token)
+      creds = pickle.load(token)
 
   # If credentials are not available or are invalid, ask the user to log in.
   if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(current_app.config['OAUTH2_TOKEN'], SCOPES)
-      creds = flow.run_local_server(port=0)
+    if user.admin:
+      if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+      else:
+        flow = InstalledAppFlow.from_client_secrets_file(current_app.config['OAUTH2_TOKEN'], SCOPES)
+        creds = flow.run_local_server(port=0)
 
-    # Save the access token in token.pickle file for the next run
-    with open('token.pickle', 'wb') as token:
-      pickle.dump(creds, token)
+      # Save the access token in token.pickle file for the next run
+      with open('token.pickle', 'wb') as token:
+        pickle.dump(creds, token)
   try:
     service = build('gmail', 'v1', credentials=creds)
     if type == 0:
