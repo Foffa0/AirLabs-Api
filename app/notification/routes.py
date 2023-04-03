63dadcfb58361096c51a45b9a4f13f4b59da9bb8
Foffa <schmuck.christopher@gmail.com>
2023-04-03 23:19:48 +0200

    handle push message tokens

--- app/notification/routes.py
+++ app/notification/routes.py
@@ -1,10 +1,11 @@
 import json
-from flask import request, Response, render_template, jsonify, Flask, Blueprint, current_app
-from google.oauth2 import service_account
-import google.auth.transport.requests
-import requests
-import time
-from firebase_admin import messaging
+from flask import request, render_template, Blueprint, flash, redirect, url_for
+from flask_login import current_user
+from app.models import Notification
+from app import db
+from flask_login import login_required
+from app.decorators import check_confirmed
+
 # if the operating machine settings are corrupted
 import mimetypes
 mimetypes.add_type('text/javascript', '.js')
@@ -13,76 +14,42 @@ notification = Blueprint('notification', __name__)
 
 @notification.route('/notify')
 def index():
-  
   return render_template('public/web_push.html')
 
 @notification.route('/firebase-messaging-sw.js')
 def service_worker():
-    from flask import make_response, send_from_directory
-    response = make_response(send_from_directory(directory='static',path='firebase-messaging-sw.js'))
-    response.headers['Content-Type'] = 'application/javascript'
-    response.headers['Service-Worker-Allowed'] = '/'
-    return response
+  from flask import make_response, send_from_directory
+  response = make_response(send_from_directory(directory='static',path='firebase-messaging-sw.js'))
+  response.headers['Content-Type'] = 'application/javascript'
+  response.headers['Service-Worker-Allowed'] = '/'
+  return response
 
-@notification.route('/postmethod', methods = ['POST'])
-def get_post_javascript_data():
+@notification.route('/add-token', methods = ['POST'])
+@login_required
+@check_confirmed
+def get_device_token():
   registration_token = json.loads(request.data)['token']
 
-  print(registration_token)
-  # See documentation on defining a message payload.
+  device = Notification.query.filter_by(token=registration_token).first()
+  if device:
+    flash('Token already in database!', 'warning')
+    return redirect(url_for('main.alerts'))
 
-  _send_fcm_message(_build_common_message(registration_token))  
+  device = Notification(token=registration_token, user_id=current_user.id)
+  db.session.add(device)
+  db.session.commit()
   return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
-PROJECT_ID = 'flightalert-380817'
-BASE_URL = 'https://fcm.googleapis.com'
-FCM_ENDPOINT = 'v1/projects/' + PROJECT_ID + '/messages:send'
-FCM_URL = BASE_URL + '/' + FCM_ENDPOINT
-SCOPES = ['https://www.googleapis.com/auth/firebase.messaging']
-
-def _get_access_token():
-  """Retrieve a valid access token that can be used to authorize requests.
-
-  :return: Access token.
-  """
-  credentials = service_account.Credentials.from_service_account_file(current_app.config['GOOGLE_APPLICATION_CREDENTIALS'], scopes=SCOPES)
-  request = google.auth.transport.requests.Request()
-  credentials.refresh(request)
-  return credentials.token
-
-
-def _send_fcm_message(fcm_message):
-  """Send HTTP request to FCM with given message.
-  Args:
-    fcm_message: JSON object that will make up the body of the request.
-  """
-  # [START use_access_token]
-  headers = {
-    'Authorization': 'Bearer ' + _get_access_token(),
-    'Content-Type': 'application/json; UTF-8',
-  }
-  # [END use_access_token]
-  resp = requests.post(FCM_URL, data=json.dumps(fcm_message), headers=headers)
-
-  if resp.status_code == 200:
-    print('Message sent to Firebase for delivery, response:')
-    print(resp.text)
-  else:
-    print('Unable to send message to Firebase')
-    print(resp.text)
-
-def _build_common_message(token):
-  """Construct common notifiation message.
-  Construct a JSON object that will be used to define the
-  common parts of a notification message that will be sent
-  to any app instance subscribed to the news topic.
-  """
-  return {
-    'message': {
-      'token': token,
-      'notification': {
-        'title': 'FCM Notification',
-        'body': 'Notification from FCM'
-      }
-    }
-  }
+@notification.route('/remove-token', methods = ['POST'])
+@login_required
+@check_confirmed
+def remove_device_token():
+  registration_token = json.loads(request.data)['token']
+  try:
+    device = Notification.query.filter_by(token=registration_token).first()
+    if not device is None:
+      db.session.delete(device)
+      db.session.commit()
+  except:
+    pass
+  return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
