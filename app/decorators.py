907909c1009a9a6b6f7f0e3f066a5005e471b9a6
Foffa <schmuck.christopher@gmail.com>
2023-04-12 22:57:45 +0200

    add admin decorator

--- app/decorators.py
+++ app/decorators.py
@@ -1,6 +1,6 @@
 from functools import wraps
 
-from flask import flash, render_template
+from flask import flash, render_template, abort
 from flask_login import current_user
 
 
@@ -10,6 +10,15 @@ def check_confirmed(func):
         if current_user.confirmed is False:
             flash('Please confirm your account!', 'warning')
             return render_template('public/activate_account.html')
+        return func(*args, **kwargs)
+
+    return decorated_function
+
+def admin_required(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        if current_user.admin is False:
+            return abort(403)
         return func(*args, **kwargs)
 
     return decorated_function
